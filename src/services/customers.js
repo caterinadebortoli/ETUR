9412bdc9a2680a99fca2bbdb0eced3312e833135
Jannic Heidrich <heidrich.jannic@gmx.de>
2024-02-12 15:58:59 +0100

    Edit Create

--- src/services/customers.js
+++ src/services/customers.js
@@ -5,24 +5,14 @@ export default class Customer {
     }
   }
   export function Create(){
-    const readline = require("readline");
-  
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
   
     const customerName = "";
     const customerNumber = "";
   
-    rl.question("Type in the Customer Name:", function (string) {
-      customerName = string;
-      rl.close();
-    });
-    rl.question("Type in the Customer Nummber:", function (string){
-      customerNumber = string;
-      rl.close();
-    })
+    const question = prompt("Type in the Customer Name:", customerName);
+    console.log(question);
+    question = prompt("Type in the Customer Number:", customerNumber);
+    console.log(question);
   
     const customer = new Customer(customerName,customerNumber)
     customers.push(customer)
@@ -33,27 +23,6 @@ export default class Customer {
     return customer
   }
 
-<<<<<<< HEAD
-export function Create(){
-  const customerName = "";
-  const customerNumber = "";
-
-  const question = prompt("Type in the Customer Name:", customerName);
-  console.log(question);
-  question = prompt("Type in the Customer Number:", customerNumber);
-  console.log(question);
-
-  const customer = new Customer(customerName,customerNumber)
-  customers.push(customer)
-}  
-
-export function readcustomer(id){
-  let customer = customers.find(customer=>customer.id==id)
-  return customer
-}
-=======
-
->>>>>>> 9447ef751f5ccfaa8ab2f7a32feb69ff2c4d7428
   export const customers=[]
  
 
