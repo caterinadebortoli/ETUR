9447ef751f5ccfaa8ab2f7a32feb69ff2c4d7428
Caterina De Bortoli <Caterina.DeBortoli@plan-b-gmbh.com>
2024-02-12 15:55:23 +0100

    readcustomer

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 import Customer from './customers.js';
 import { customers } from './customers.js';
 import { Create } from './customers.js';
-
+import { readcustomer } from './customers.js';
 Create();
 function showcustomers(){
     return customers
 }
+let idinput = prompt("give the id of the customer you would like to read: ")
+console.log(readcustomer(idinput))
 console.log(showcustomers())
