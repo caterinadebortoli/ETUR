fbf9d311035075afbe8e1d2a88c6cd89746a0063
Jannic Heidrich <heidrich.jannic@gmx.de>
2024-02-12 15:56:38 +0100

    Add Create

--- src/services/customers.js
+++ src/services/customers.js
@@ -6,24 +6,13 @@ export default class Customer {
   }
 
 export function Create(){
-  const readline = require("readline");
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
   const customerName = "";
   const customerNumber = "";
 
-  rl.question("Type in the Customer Name:", function (string) {
-    customerName = string;
-    rl.close();
-  });
-  rl.question("Type in the Customer Nummber:", function (string){
-    customerNumber = string;
-    rl.close();
-  })
+  const question = prompt("Type in the Customer Name:", customerName);
+  console.log(question);
+  question = prompt("Type in the Customer Number:", customerNumber);
+  console.log(question);
 
   const customer = new Customer(customerName,customerNumber)
   customers.push(customer)
