987e152f377386e18a05a980a81da41e068ab71a
Caterina De Bortoli <Caterina.DeBortoli@plan-b-gmbh.com>
2024-02-12 16:26:21 +0100

    now it works

--- src/services/index.js
+++ src/services/index.js
@@ -2,13 +2,13 @@ import Customer from './customers.js';
 import { customers } from './customers.js';
 import { Create } from './customers.js';
 import { readcustomer } from './customers.js';
+import readlineSync from 'readline-sync';
 
 function showcustomers(){
     return customers
 }
 
 
-const readlineSync = require('readline-sync');
 const customerName = readlineSync.question('Type in the Customer Name: ');
 const customerNumber = readlineSync.question('Type in the Customer Number: ');
 Create(customerName, customerNumber)
