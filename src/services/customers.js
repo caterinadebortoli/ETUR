03fff2934a74f39ce544abb94cd198686c087b98
Caterina De Bortoli <Caterina.DeBortoli@plan-b-gmbh.com>
2024-02-12 16:36:08 +0100

    delete function added

--- src/services/customers.js
+++ src/services/customers.js
@@ -14,6 +14,25 @@ export default class Customer {
     return customer
   }
 
+
+
+
+export function deleteCustomerById(id) {
+    // Find the index of the customer with the given ID
+    const index = customers.findIndex(customer => customer.number === id);
+
+    // If customer with the given ID exists, remove it from the array
+    if (index !== -1) {
+        customers.splice(index, 1);
+        console.log(`Customer with ID ${id} deleted successfully.`);
+    } else {
+        console.log(`Customer with ID ${id} not found.`);
+    }
+}
+
+
+
+
   export const customers=[]
  
 
--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,7 @@ import { customers } from './customers.js';
 import { Create } from './customers.js';
 import { readcustomer } from './customers.js';
 import readlineSync from 'readline-sync';
-
+import { deleteCustomerById } from './customers.js';
 
 
 const customerName = readlineSync.question('Type in the Customer Name: ');
@@ -11,3 +11,4 @@ const customerNumber = readlineSync.question('Type in the Customer Number: ');
 Create(customerName, customerNumber)
 const idinput = readlineSync.question('Type in the Customer id: ');
 console.log(readcustomer(idinput))
+deleteCustomerById(idinput)
