2a8825400b18199905d21c5af7968fd707f37484
Jannic Heidrich <heidrich.jannic@gmx.de>
2024-02-12 16:38:45 +0100

    ff

--- src/services/customers.js
+++ src/services/customers.js
@@ -10,28 +10,9 @@ export default class Customer {
   }  
   
   export function readcustomer(id){
-    let customer = customers.find(customer=>customer.number==id)
+    let customer = customers.find(customer=>customer.id==id)
     return customer
   }
-
-
-
-
-export function deleteCustomerById(id) {
-    // Find the index of the customer with the given ID
-    const index = customers.findIndex(customer => customer.number === id);
-
-    // If customer with the given ID exists, remove it from the array
-    if (index !== -1) {
-        customers.splice(index, 1);
-        console.log(`Customer with ID ${id} deleted successfully.`);
-    } else {
-        console.log(`Customer with ID ${id} not found.`);
-    }
-}
-
-
-
 
   export const customers=[]
  
