7c4fab2d63ad84eaf07b9b3d1d05860ea6d43d4d
Caterina De Bortoli <Caterina.DeBortoli@plan-b-gmbh.com>
2024-02-12 15:19:41 +0100

    new class Customer

---
+++ src/services/customers.js
@@ -0,0 +1,11 @@
+class Customer {
+    constructor(name, number) {
+      this.name = name;
+      this.number = number;
+    }
+  }
+  
+  const customerZeiss = new Customer("Zeiss","z2415")
+
+  const customers=[]
+
