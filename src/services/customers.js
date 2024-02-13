a4fb3eb48e1d881e88183157b5e035401f337598
Jannic Heidrich <heidrich.jannic@gmx.de>
2024-02-13 11:05:59 +0100

    Changed the functions

--- src/services/customers.js
+++ src/services/customers.js
@@ -19,19 +19,20 @@
   }
 
   export function doesCostumerExist(id){
-    return customers.some(customer=>customer.number==id)
+    return customers.some(customer=>customer.number===id)
   }
 
   export function CreateCustomer(id,customerName){
     let is_valid=validateid(id)
     let does_exist=doesCostumerExist(id)
-    if (is_valid && does_exist==false)
+    if (is_valid && does_exist===false)
     {
       const customer = new Customer(customerName,id)
       customers.push(customer)    }
     else
     {
       console.log("The id given is not valid")
+      throw new Error("The id given is not valid")
     }
   }
 
@@ -43,32 +44,106 @@
       console.log(`Customer with Number ${id} deleted successfully.`);
     } else {
       console.log(`Customer with Number ${id} not found.`);
+      throw new Error("Customer with Number your id not found.")
     }
   }
 
   export function readcustomer(id){
-    let customer = customers.find(customer=>customer.id==id)
+    let customer = customers.find(customer=>customer.number===id)
+    if(customer===undefined)
+    {
+      throw new Error("Customer not found");
+    }
+    else
+    {
     return customer
+    }
   }
 
   export function showallcustomers(){
     return customers
   }
 
+//#region Schema's
+
+  const getcustomerSchema = {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties:{
+            customer: { type : 'object'}
+          }
+          
+        }
+      }
+    }
+  }
+
+  const customerSchema = {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          id: { type: 'string' },
+        },
+        required: ['name','id']
+      },
+      response: {
+        201: {
+          type: 'string',
+          
+        }
+      }
+    }
+  }
+
+  const deleteSchema = {
+    schema: {
+      response:{
+        200: {
+          type: 'string',
+        }
+      }      
+    }
+  }
+  
+//#endregion
+
   export async function customerRoutes(fastify, options) {
     fastify.get("/customers", async (request, reply) => {
       return showallcustomers()
     });
   
-    fastify.get("/customers/:id", async (request, reply) => {
-      return readcustomer(request.params.id)
+    fastify.get("/customers/:id",getcustomerSchema, async (request, reply) => {
+      try{
+       let customer=readcustomer(request.params.id)
+       reply.code(200).send({customer:customer})
+      }
+        catch(err){
+          reply.code(500).send(err)
+        }
     });
    
-    fastify.post("/customers",async (request, reply)=>{
-      return CreateCustomer(request.body.id,request.body.name )
+    
+    fastify.post("/customers",customerSchema, async (request, reply)=>{
+      try{
+        CreateCustomer(request.body.id,request.body.name )
+        reply.code(201).send('Customer successfully created');
+      } 
+      catch(err) {
+        reply.code(500).send(err);
+      }
     })
 
-    fastify.delete("/customers/:id", async (request, reply)=>{
-      return deleteCustomerById(request.params.id)
+    fastify.delete("/customers/:id",deleteSchema, async (request, reply)=>{
+      try{
+
+        reply.code(200).send("Customer deleted successfully");
+      }    
+      catch(err){
+        reply.code(500).send(err);
+      }
     })
   }
